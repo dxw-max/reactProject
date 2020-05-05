c35e647b2c57b2b2451f64c564c8060925539d3a
liminpeng <1006381441@qq.com>
2020-05-05 23:03:45 +0800

    日志

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/store'
 import "lib-flexible"
+import "lib-flexible";
 
 import {
     Provider
